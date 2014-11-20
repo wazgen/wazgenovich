c2b461cdb79a4fa941422e29fafce203be99caac
User <User@10.11.184.8>
2014-11-20 14:14:42 +0400

    Commit 2

--- JavaApplication7/src/javaapplication7/JavaApplication7.java
+++ JavaApplication7/src/javaapplication7/JavaApplication7.java
@@ -15,7 +15,7 @@ public class JavaApplication7 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello, World!!!");
     }
     
 }
